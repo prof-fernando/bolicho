7758add034ae05e878516d5e712467d2ca17269a
Fernando <fernando.oliveira@iffarroupilha.edu.br>
2017-04-27 22:17:21 -0300

    adicionando projeto web

---
+++ bolicho.visao.web/src/br/edu/iffarroupilha/bolicho/visao/web/ClienteController.java
@@ -0,0 +1,13 @@
+package br.edu.iffarroupilha.bolicho.visao.web;
+
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class ClienteController {
+
+	
+	public void listar(){
+		System.out.println("aqui vamos buscar os clientes!");
+	}
+	
+}
