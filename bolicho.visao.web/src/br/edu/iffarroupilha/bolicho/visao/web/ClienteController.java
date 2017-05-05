0f8cb6b2bce5d70ce0c9418ad1e8deeaae24dfde
Fernando <fernando.oliveira@iffarroupilha.edu.br>
2017-05-04 22:25:24 -0300

    enviando alterações do dia! Gravando cliente

--- bolicho.visao.web/src/br/edu/iffarroupilha/bolicho/visao/web/ClienteController.java
+++ bolicho.visao.web/src/br/edu/iffarroupilha/bolicho/visao/web/ClienteController.java
@@ -1,6 +1,8 @@
 package br.edu.iffarroupilha.bolicho.visao.web;
 
 import br.com.caelum.vraptor.Resource;
+import br.edu.iffarroupilha.bolicho.controle.ClienteControle;
+import br.edu.iffarroupilha.bolicho.modelo.Cliente;
 
 @Resource
 public class ClienteController {
@@ -8,6 +10,19 @@ public class ClienteController {
 	
 	public void listar(){
 		System.out.println("aqui vamos buscar os clientes!");
+	}
+	
+	public void gravar(){
+		ClienteControle controle = new ClienteControle();
+		
+		// objeto temporario
+		Cliente c = new Cliente();
+		c.setNome("Jose");
+		c.setCpf(123);
+		
+		controle.gravar( c  );
+		
+		
 	}
 	
 }
